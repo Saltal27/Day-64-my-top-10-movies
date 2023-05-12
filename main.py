04c595cb8b97ca977f9564a245b176be20fb7693
Saltal27 <omarmobarak53@gmail.com>
2023-05-12 18:58:17 +0300

    Added explanatory comments

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ from wtforms.validators import DataRequired, Length
 import imdb
 import http.client
 
-
+# Create a Flask app and configure it
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap(app)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+
+# Initialize the Flask extensions
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
 
-# Create the movies table in the database
+# Define the Movie model
 class Movie(db.Model):
     __tablename__ = 'movies'
     id = db.Column(db.Integer, primary_key=True)
@@ -29,13 +30,14 @@ class Movie(db.Model):
     img_url = db.Column(db.String, nullable=False)
 
 
-# Flask-WTForms
+# Define the form to rate a movie
 class RateMovieForm(FlaskForm):
     rating = StringField('Your Rating Out of 10 e.g. 7.5', validators=[DataRequired()])
     review = StringField('Your Review', validators=[DataRequired()])
     submit = SubmitField('Done')
 
 
+# Define the form to add a movie
 class AddMovieForm(FlaskForm):
     title = StringField('Movie Title', validators=[DataRequired(), Length(max=50)])
     submit = SubmitField('Add Movie')
@@ -64,6 +66,14 @@ class AddMovieForm(FlaskForm):
 
 # Data-changing functions
 def update_movie(movie_id, new_rating, new_review):
+    """
+    Updates an existing movie in the database with a new rating and review.
+
+    Args:
+        movie_id (int): The id of the movie to update.
+        new_rating (float): The new rating of the movie.
+        new_review (str): The new review of the movie.
+    """
     with app.app_context():
         movie = Movie.query.filter_by(id=movie_id).first()
         movie.rating = new_rating
@@ -72,6 +82,12 @@ def update_movie(movie_id, new_rating, new_review):
 
 
 def delete(movie_id):
+    """
+    Deletes a movie from the database.
+
+    Args:
+        movie_id (int): The id of the movie to delete.
+    """
     with app.app_context():
         movie_to_delete = Movie.query.get(movie_id)
         db.session.delete(movie_to_delete)
@@ -84,6 +100,12 @@ search_results = []
 
 
 def search_movies(movie_title):
+    """
+    Searches for movies using the IMDb API.
+
+    Args:
+        movie_title (str): The title of the movie to search for.
+    """
     global search_results
     # Search for the movie title using the search_movie() method
     search_results = None
@@ -97,6 +119,12 @@ def search_movies(movie_title):
 
 
 def movies_titles_list():
+    """
+    Extracts the titles of the movies from the search results.
+
+    Returns:
+        list: A list of strings containing the titles of the movies.
+    """
     global search_results
 
     movies_titles = []
@@ -107,6 +135,15 @@ def movies_titles_list():
 
 
 def get_movie_details(selection):
+    """
+    Gets the details of a movie using the IMDb API.
+
+    Args:
+        selection (int): The index of the selected movie in the search results.
+
+    Returns:
+        dict: A dictionary containing the details of the movie.
+    """
     global search_results
     # Get the movie details using the get_movie() method
     movie_id = search_results[selection - 1].getID()
@@ -122,8 +159,15 @@ def get_movie_details(selection):
     return movie_details
 
 
+# Define the routes of the Flask app
 @app.route("/")
 def home():
+    """
+    Renders the home page of the app.
+
+    Returns:
+        str: The HTML content of the home page.
+    """
     all_movies = db.session.query(Movie).all()
 
     return render_template("index.html", all_movies=all_movies)
@@ -131,6 +175,15 @@ def home():
 
 @app.route("/edit_movie/<int:movie_id>", methods=["POST", "GET"])
 def edit_movie(movie_id):
+    """
+    Renders the page to edit the rating and review of a movie.
+
+    Args:
+        movie_id (int): The id of the movie to edit.
+
+    Returns:
+        str: The HTML content of the edit page.
+    """
     update_form = RateMovieForm()
     if update_form.validate_on_submit():
         new_rating = update_form.rating.data
@@ -144,6 +197,15 @@ def edit_movie(movie_id):
 
 @app.route("/delete_movie/<int:movie_id>")
 def delete_movie(movie_id):
+    """
+    Deletes a movie from the database.
+
+    Args:
+        movie_id (int): The id of the movie to delete.
+
+    Returns:
+        str: A redirect to the home page.
+    """
     delete(movie_id)
 
     return redirect(url_for("home"))
@@ -151,6 +213,12 @@ def delete_movie(movie_id):
 
 @app.route("/add_movie", methods=["POST", "GET"])
 def add_movie():
+    """
+    Renders the page to add a movie to the database.
+
+    Returns:
+        str: The HTML content of the add page.
+    """
     add_form = AddMovieForm()
     if add_form.validate_on_submit():
         movie_title = add_form.title.data
@@ -164,10 +232,21 @@ def add_movie():
 
 @app.route("/add_movie/<int:selection>/<movie_title>", methods=["POST", "GET"])
 def select_movie(selection, movie_title):
+    """
+    Renders the page to select a movie from the search results.
+
+    Args:
+        selection (int): The index of the selected movie in the search results.
+        movie_title (str): The title of the movie to add.
+
+    Returns:
+        str: The HTML content of the movie page.
+    """
     movie_details = get_movie_details(selection)
 
     return render_template("movie.html", movie_title=movie_title, movie_details=movie_details)
 
 
+# Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
